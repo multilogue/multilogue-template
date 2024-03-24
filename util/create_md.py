643be2362e307023c3d15f79e0fb09e23738d02f
Alex Fedotov <alxfed@gmail.com>
2024-03-24 17:58:22 -0500

    Corrections

---
+++ util/create_md.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+import jsonlines
+from wireframes.continuous import simple_continuation, commented_continuation
+
+
+def load_record(self):
+    with jsonlines.open('./record.jsonl', 'r') as reader:
+        record = [line for line in reader.iter()]
+
+
+    return record
+
+
+def create_md_text(template: dict, record: dict) -> str:
+    for line in record:
+    md_text = template.format(
+        first_interlocutor_name=record['first_interlocutor_name'],
+        first_interlocutor_utterance=record['first_interlocutor_utterance']
+    )
+    md_text += template['second_interlocutor'].format(
+        second_interlocutor_utterance=record['second_interlocutor_utterance']
+    )
+    return md_text
+
+
+if __name__ == "__main__":
+    record =
+    create_md_text(template=simple_continuation)
---
+++ wireframes/continuous.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Python
+
+"""Copyright (c) Alexander Fedotov.
+This source code is licensed under the license found in the
+LICENSE file in the root directory of this source tree.
+"""
+simple_continuation = {
+    "fist_interlocutor": "\n{first_interlocutor_name}: {first_interlocutor_utterance}\n",
+    "second_interlocutor": "\n{second_interlocutor_name}: {second_interlocutor_utterance}\n"
+}
+commented_continuation = {
+    "fist_interlocutor": "\n{first_interlocutor_name}: {first_interlocutor_utterance}\n",
+    "second_interlocutor": "\n>{second_interlocutor_name}: {second_interlocutor_utterance}\n"
+}
