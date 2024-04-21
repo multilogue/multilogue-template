c24e932b12f68a17d0a6b4a4c6321488cae5b3ac
Alex Fedotov <alxfed@gmail.com>
2024-04-21 14:52:06 -0500

    Corrections

--- main.py
+++ main.py
@@ -5,11 +5,21 @@
 This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
+import multilogue as multi
+import meeting.assemble as assemble
 
 
 def main():
+    assemble.gather()
     pass
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        while True:
+            # This code may be interrupted by ^C on the keyboard.
+            main()
+    except KeyboardInterrupt:
+        # Clean shutdown
+        print("Keyboard Interrupt detected. Exiting...")
+        exit(0)
